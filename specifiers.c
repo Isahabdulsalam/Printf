023bee88b6bd904401902f5dd0f8f93faf6421a1
Isah Abdulsalam <isahabdulsalam416@gmail.com>
2023-11-14 16:35:30 +0100

    fun

---
+++ specifiers.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdarg.h>
+
+int _printf(const char *format, ...)
+{
+	va_list argument;
+	va_start(argument, format, ...);
+
+	int count = 0;
+
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 'c')
+			{
+				int character = va_arg(argument, int);
+				_putchar(character);
+				count++;
+			}
+			else if (*format == 's')
+			{
+				const char *string = va_arg(argument, const char*);
+				while (*string != '\0')
+				{
+					_putchar(*string);
+					string++;
+					count++;
+				}
+				else if (*format == '%')
+				{
+					_putchar('%');
+					count++;
+				}
+				else
+				{
+					_putchar(*format);
+					count++;
+				}
+				format++;
+			}
+		}
+	}
+	va_end(argument);
+	return (count);
+}
