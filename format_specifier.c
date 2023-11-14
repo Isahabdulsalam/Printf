c4e7f56abef9e86c7d9f69c87af9f1b2adc151c5
Isah Abdulsalam <isahabdulsalam416@gmail.com>
2023-11-14 20:26:23 +0100

    format

--- format_specifier.c
+++ format_specifier.c
@@ -2,13 +2,12 @@
 #include <stdarg.h>
 #include <stdio.h>
 
-
-int custom_printf(const char *format, ...) {
-    va_list args;
-    va_start(args, format);
+int _spec(const char *format, ...) {
 
     int num = 0;
     int count = 0;
+	va_list args;
+    va_start(args, format);
 
     while (*format != '\0') {
         if (*format == '%' && (*(format + 1) == 'd' || *(format + 1) == 'i')) {
